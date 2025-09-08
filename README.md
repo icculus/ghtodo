7f1f19c573dea22756e94700d3d44956b925b6b3
Ryan C. Gordon <icculus@icculus.org>
2025-09-08 09:50:43 -0400

    Initial add.

---
+++ README.md
@@ -0,0 +1,92 @@
+# ghtodo
+
+## What is this?
+
+This is a simple Perl script to submit new bugs to a GitHub issue tracker.
+This isn't really meant for _bug reporting_, but for quickly adding tasks to a
+TODO list.
+
+## Setup
+
+Make a GitHub repository where you'll file bugs with this script. I called
+mine "TODO" and made it private.
+
+This script does not need to be installed system-wide. However, it needs the
+the Net::GitHub module installed:
+
+Ubuntu/Debian users can install this with:
+
+```bash
+sudo apt-get install libnet-github-perl
+```
+
+Other distros likely have similar packages, check your package manager. If all
+else fails, you can try forcing the issue with CPAN:
+
+```bash
+sudo perl -MCPAN -e 'install Net::GitHub;'
+```
+
+You'll need a GitHub Personal Access Token. These are free and created through
+the GitHub web interface.
+
+Go here: https://github.com/settings/personal-access-tokens/new
+
+Give the token a name like "todo-list", set repository access to
+"All repositories", click "Add permissions" and select "Issues" in the popup.
+
+Add whatever description and expiration you like.
+
+Click "Generate token" and copy down the long string of characters it'll
+display. This won't be displayed again!
+
+Make a file named ".ghtodo" in your home directory. Put this in it:
+
+```
+username=MyGithubUsername
+repo=MyGithubRepoName
+token=github_pat_[whatver your long string of characters from your GitHub token goes here]
+```
+
+Put the script in your PATH, or make an alias, like I did:
+
+```bash
+alias todo $HOME/projects/ghtodo/ghtodo.pl
+```
+
+Then run it:
+
+```bash
+ghtodo.pl "Take out the trash"
+```
+
+This will pop up a text editor if you want to write more about this task. If
+you don't, just close the text editor.
+
+If you want to leave a description without a text editor:
+
+```bash
+ghtodo.pl "Take out the trash" "You forgot last week..."
+```
+
+To leave _no_ description and not be bothered by it:
+
+```bash
+ghtodo.pl "Take out the trash" --
+```
+
+(I suppose an empty string will work here instead of `--`, too.)
+
+A single dash will read the description from stdin:
+
+```bash
+echo "You forgot last week..." |ghtodo.pl "Take out the trash" -
+```
+
+And that's it. It'll post a new issue and print the URL.
+
+
+## Questions and problems
+
+File a bug. Through the web interface, not this script.  :)
+
---
+++ ghtodo.pl
@@ -0,0 +1,147 @@
+#!/usr/bin/perl -w
+
+# sudo apt-get install libnet-github-perl
+
+use warnings;
+use strict;
+use Net::GitHub;
+use Net::GitHub::V3;
+
+binmode STDOUT, ':utf8';
+binmode STDERR, ':utf8';
+
+# Globals...
+my $github = undef;  # Access handle for talking to GitHub (v3 API).
+my $home = $ENV{'HOME'};
+my $use_desc_file = 0;
+my $desc_file_path = "$home/.ghtodo_desc.md";
+
+# Config/commandline stuff...
+my $title = undef;
+my $description = undef;
+my $github_token = undef;
+my $github_username = undef;
+my $github_reponame = undef;
+my $editor = undef;
+
+sub usage {
+    print STDERR "USAGE: $0 <new_bug_title> [description]\n";
+    exit(1);
+}
+
+sub parse_config_file {
+    my $path = (defined $ENV{'GHTODO_CFGPATH'}) ? $ENV{'GHTODO_CFGPATH'} : $ENV{'HOME'} . '/.ghtodo';
+    open(CFGIN, '<', $path) or die("Couldn't open '$path': $!\n");
+    while (<CFGIN>) {
+        chomp;
+        s/\A\s+//;
+        s/\s+\Z//;
+        next if /\A\#/;
+        if (/\A(.*?)\s*\=\s*(.*?)\Z/) {
+            my $k = $1;
+            my $v = $2;
+            if ($k eq 'username') {
+                $github_username = $v;
+            } elsif ($k eq 'repo') {
+                $github_reponame = $v;
+            } elsif ($k eq 'token') {
+                $github_token = $v;
+            } elsif ($k eq 'editor') {
+                $editor = $v;
+            } else {
+                die("Unknown config key '$k' in '$path'\n");
+            }
+        } else {
+            die("Malformed config line in '$path'");
+        }
+    }
+    close(CFGIN);
+
+    $editor = $ENV{'EDITOR'} if not defined $editor;
+}
+
+sub parse_commandline {
+    foreach (@ARGV) {
+        $github_username = $1, next if (/\A\-\-username\=(.*)\Z/);
+        $github_reponame = $1, next if (/\A\-\-repo\=(.*)\Z/);
+        $github_token = $1, next if (/\A\-\-token\=(.*)\Z/);
+        $editor = $1, next if (/\A\-\-editor\=(.*)\Z/);
+        $title = $_, next if not defined $title;
+        $description = $_, next if not defined $description;
+        usage();
+    }
+    usage() if not defined $title;
+}
+
+sub auth_to_github {
+    $github = Net::GitHub::V3->new(version => 3, access_token => $github_token, RaiseError => 1) or die("Failed to connect to GitHub v3: $!\n");
+    #$github4 = Net::GitHub::V4->new(version => 4, access_token => $github_token, RaiseError => 1) or die("Failed to connect to GitHub v4: $!\n");
+    my $u = $github->user->show();   # just so we have to talk to them at all.
+    $github->set_default_user_repo($github_username, $github_reponame);
+}
+
+sub prepare_descriotion {
+    if ((defined $description) and ($description eq '-')) {
+        $description = '';
+        while (<STDIN>) {
+            $description .= $_;
+        }
+        return;
+    }
+
+    return if defined $description;
+
+    $use_desc_file = 1;
+    my $path = $desc_file_path;
+    if (! -f $path) {
+        open(FH, '>', $path) or die("Can't open '$path': $!\n");
+        print FH qq{
+
+
+<!-- Everything below this line is ignored.
+Write any description for the new issue above, in Markdown.
+Leave this file blank to not provide a description. -->
+
+};
+
+        close(FH);
+    }
+
+    (system("$editor '$path'") == 0) or die("Launching '$editor' seems to have failed: $!\n");
+
+    open(FH, '<', $path) or die("Couldn't open '$path': $!\n");
+    $description = '';
+    while (<FH>) {
+        last if /\A\<\!\-\- Everything below this line is ignored\./;
+        $description .= $_;
+    }
+    close(FH);
+}
+
+sub verify_config {
+    die("Missing 'username' in config or commandline\n") if (not defined $github_username);
+    die("Missing 'repo=' line in config or commandline\n") if (not defined $github_reponame);
+    die("Missing 'token=' line in config or commandline\n") if (not defined $github_token);
+    die("Missing 'editor=' line in config or commandline (or EDITOR env var)\n") if (not defined $editor);
+}
+
+sub post_new_issue {
+    my $issue = $github->issue->create_issue( { "title" => $title, "body" => $description } );
+    unlink($desc_file_path) if ($use_desc_file);
+    my $issue_number = int($$issue{'number'});
+    print("https://github.com/$github_username/$github_reponame/issues/$issue_number\n");
+}
+
+
+# Mainline!
+
+parse_config_file();
+parse_commandline();
+verify_config();
+prepare_descriotion();
+auth_to_github();
+post_new_issue();
+
+# end of ghtodo.pl ...
+
+
